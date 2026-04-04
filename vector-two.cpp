0898f517cf69c6a1b6938d9894e8210f8e7294f1
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-04-04 22:36:23 +0600

    class-1: vector two

--- vector-two.cpp
+++ vector-two.cpp
@@ -5,7 +5,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout<<"hello";
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    vector<int> a;
+    a.push_back(10);
+    a.push_back(20);
+    a.push_back(30);
+
+    // v.insert(v.begin(), 20);
+    // v.insert(v.begin()+3, 3, 10);
+
+    // swap two vector
+    // swap(a, v);
+
+    // sort(v.begin(), v.end());
+    // reverse(v.begin(), v.end());
+
+    // iterator
+    vector<int>::iterator it;
+    // it = v.begin()+2;
+    // cout<<*it<<'\n';
+
+    for(it=v.begin(); it!=v.end(); it++) {
+        cout<<*it<<' ';
+    }
+
+    // for(int i=0;i<v.size();i++) cout<<v[i]<<' ';
+    // cout<<'\n';
 
     return 0;
 }
