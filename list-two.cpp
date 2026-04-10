4fee21f5ecfd81c333d4f3ce498e3415927863b2
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-04-10 23:32:13 +0600

    list-two

--- list-two.cpp
+++ list-two.cpp
@@ -5,7 +5,27 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cout<<"hello\n";
+    list<int> li = {10, 10, 30, 40, 50};
+    list<int> li2 = {1, 2, 3, 4, 5};
+
+    // list<int>:: iterator it, it2;
+    // it = li.begin();
+    // it2 = li.begin();
+    // advance(it, 1);
+    // advance(it2, 3);
+
+    // li.erase(it, it2);
+
+    // li.remove(10);
+    // li.reverse();
+    // li.sort();
+
+    // li.unique();
+
+    li.merge(li2);
+
+
+    for(auto it:li) cout<<it<<' ';
 
     return 0;
 }
