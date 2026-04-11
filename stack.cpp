b675cde3065b5cd5cee7400690387db5c11d440a
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-04-11 15:10:33 +0600

    stack

---
+++ stack.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+
+    while(!st.empty()) {
+        cout<<st.top()<<' ';
+        st.pop();
+    }
+
+    return 0;
+}
