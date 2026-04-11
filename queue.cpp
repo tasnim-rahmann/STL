d01df91a160e6b395a99a0c895bd95104717567a
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-04-11 14:54:22 +0600

    queue

---
+++ queue.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    queue<int> q;
+    // q.push(1);
+    // q.push(2);
+    // q.push(3);
+    // q.push(4);
+    // q.push(5);
+
+    q.emplace(1);
+    q.emplace(2);
+    q.emplace(3);
+    q.emplace(4);
+
+    q.pop();
+
+    while(!q.empty()) {
+        cout<<q.front()<<' ';
+        q.pop();
+    }
+
+    return 0;
+}
