d2a649b2aade951301c937661682b51693da6876
tasnim-rahman <tasnimrahman0101@gmail.com>
2026-04-20 20:34:13 +0600

    pair

---
+++ pair.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    pair<string, int> p;
+    
+    p.first = "Tashii";
+    p.second = 15;
+
+    cout<<p.first<<' '<<p.second<<'\n';
+
+    return 0;
+}
